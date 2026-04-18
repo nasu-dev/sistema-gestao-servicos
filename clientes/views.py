ef8e648629aded77b805ad4f712b862a79de5927
Anderson <andersonnasu.dev@gmail.com>
2026-04-19 00:34:08 +0900

    feat:implementa listagem de clientes fora do admin

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Cliente
 
-# Create your views here.
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'clientes/listar.html', {'clientes': clientes})
