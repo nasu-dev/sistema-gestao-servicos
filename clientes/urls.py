0a7afe8a3b44cd563f0ca25cc772a15f88afb7ee
Anderson <andersonnasu.dev@gmail.com>
2026-04-19 10:47:33 +0900

    feat: implementa cadastro de clientes via interface

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.listar_clientes, name='Listar_clientes'),
+    path('cadastrar/', views.cadastrar_cliente, name=('cadastrar')),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -4,3 +4,14 @@ from .models import Cliente
 def listar_clientes(request):
     clientes = Cliente.objects.all()
     return render(request, 'clientes/listar.html', {'clientes': clientes})
+
+def cadastrar_cliente(request):
+    print(request.method)
+    print(request.POST)
+
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        telefone = request.POST.get('telefone')
+        if nome and telefone:
+            Cliente.objects.create(nome=nome, telefone=telefone)
+    return render(request, 'clientes/cadastrar.html')
