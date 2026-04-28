c639d2581d0dd295376f96384cfcaf1355911ec7
Anderson <andersonnasu.dev@gmail.com>
2026-04-28 20:05:56 +0900

    feat(views): adiciona feedback e protege exclusão com POST

--- clientes/views.py
+++ clientes/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cliente
 from .forms import ClienteForm
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 
 def listar_clientes(request):
     clientes = Cliente.objects.all()
     return render(request, 'clientes/listar.html', {'clientes': clientes})
+
 
 def cadastrar_cliente(request):
     if request.method == 'POST':
@@ -15,25 +17,33 @@ def cadastrar_cliente(request):
             form.save()
             messages.success(request, 'Cliente cadastrado com sucesso!')
             return redirect('listar_clientes')
+        else:
+            messages.error(request, 'Verifique os campos em vermelho')
+
     else:
         form = ClienteForm()
 
-    return render(request, 'clientes/form.html', {'form': form, 'titulo': 'Cadastrar Clientes'})
+    return render(request, 'clientes/form.html', {
+        'form': form,
+        'titulo': 'Cadastrar Clientes'
+    })
 
 def editar_cliente(request, id):
     cliente = get_object_or_404(Cliente, id=id)
-
     if request.method == 'POST':
         form = ClienteForm(request.POST, instance=cliente)
         if form.is_valid():
             form.save()
             messages.success(request, 'Cliente atualizado com sucesso!')
             return redirect('listar_clientes')
+        else:
+            messages.error(request, 'Verifique os campos em vermelho')
     else:
         form = ClienteForm(instance=cliente)
 
     return render(request, 'clientes/form.html', {'form': form, 'titulo': 'Editar Cliente'})
 
+@require_POST
 def excluir_cliente(request, id):
     cliente = get_object_or_404(Cliente, id=id)
     cliente.delete()
